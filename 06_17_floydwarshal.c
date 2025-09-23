7453c9ccd649e559e03423553f4bb00b7383ba96
Kumar Souvik Chatterjee <110775890+2003souvik@users.noreply.github.com>
2025-09-23 13:39:37 +0530

    set6

---
+++ 06_17_floydwarshal.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+#define V 4
+#define INF 99999
+
+void floydWarshall(int graph[V][V]) {
+    int dist[V][V], i, j, k;
+
+    // Initialize distance matrix
+    for (i = 0; i < V; i++)
+        for (j = 0; j < V; j++)
+            dist[i][j] = graph[i][j];
+
+    // Update distances considering each vertex as intermediate
+    for (k = 0; k < V; k++) {
+        for (i = 0; i < V; i++) {
+            for (j = 0; j < V; j++) {
+                if (dist[i][k] != INF && dist[k][j] != INF
+                    && dist[i][k] + dist[k][j] < dist[i][j])
+                    dist[i][j] = dist[i][k] + dist[k][j];
+            }
+        }
+    }
+
+    // Print the shortest distance matrix
+    printf("Shortest distance matrix:\n");
+    for (i = 0; i < V; i++) {
+        for (j = 0; j < V; j++) {
+            if (dist[i][j] == INF)
+                printf("%7s", "INF");
+            else
+                printf("%7d", dist[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int graph[V][V] = {
+        {0,   5,  INF, 10},
+        {INF, 0,   3, INF},
+        {INF, INF, 0,   1},
+        {INF, INF, INF, 0}
+    };
+
+    floydWarshall(graph);
+
+    return 0;
+}
